03fe39af5490b45a5e4f7e51e2d71c6a0510f1ab
Laur401 <laurynastautvydas11@gmail.com>
2025-01-10 01:06:38 +0100

    Added base UI elements, Pocketfold layout, layout selectors with preview layouts, array-based layout configurations, fixed a bug with incorrect page placements.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,22 @@ import {CanvasLayout, PDFtoIMG} from "./ImageConverter.tsx";
 import '@mantine/core/styles.css';
 import '@mantine/dropzone/styles.css';
 
-import {MantineProvider, DEFAULT_THEME, Text, Button, Divider, LoadingOverlay, Box} from '@mantine/core';
+import {
+    MantineProvider,
+    DEFAULT_THEME,
+    Text,
+    Button,
+    Divider,
+    LoadingOverlay,
+    Box,
+    AppShell,
+    AppShellHeader, AppShellMain, Group, Stack, Container, BackgroundImage, Title, Select, Image
+} from '@mantine/core';
 import {Dropzone, FileWithPath, MIME_TYPES} from "@mantine/dropzone";
 import FilePreview from "./FilePreview.tsx";
+import HeroBackground from "./assets/HeroBackground.jpg";
+import PocketmodLayout from "./assets/PocketmodLayout.png"
+import PocketfoldLayout from "./assets/PocketfoldLayout.png"
 
 async function downloadImage(imageURL: string) {
     const link = document.createElement("a");
@@ -21,14 +34,16 @@ function App() {
     const [imageURL, setImageURL] = useState<string|undefined>(undefined);
     const [imageIsLoading, setImageIsLoading] = useState<boolean>(false);
     const [fileSubmitted, setFileSubmitted] = useState<boolean>(false);
+    const [layoutSubmission, setLayoutSubmission] = useState<string|null>(null);
     const addFiles = (newFiles: FileWithPath[])=>{
         setFiles(files.concat(newFiles));
     };
-    const createPocketfile = async () => {
+    const createPocketfile = async (layout: string|null) => {
         //TODO: Write check for whether they uploaded a PDF or image files
+        if (layout==null) return;
         setImageIsLoading(true);
         const filesURL=[];
-        for (const file of files){
+        for (const file of files) {
             if (file.type == MIME_TYPES.pdf) {
                 const convertedFiles = await PDFtoIMG(URL.createObjectURL(file));
                 for (const convFile of convertedFiles){
@@ -37,7 +52,8 @@ function App() {
             }
             else filesURL.push(URL.createObjectURL(file))
         }
-        const pocketfile = await CanvasLayout(filesURL);
+        const pocketfile = await CanvasLayout(filesURL, layout);
+        if (pocketfile==undefined) return;
         const pocketfileBlob = new Blob([pocketfile], { type: "image/png" });
         setImageURL(URL.createObjectURL(pocketfileBlob));
         setImageIsLoading(false);
@@ -47,31 +63,72 @@ function App() {
     };
     return (
         <MantineProvider theme={DEFAULT_THEME} defaultColorScheme={"dark"}>
-            <div>
-                <Dropzone onDrop={addFiles} accept={[MIME_TYPES.png,MIME_TYPES.jpeg,MIME_TYPES.pdf]}>
-                    <div>
-                        <Text size={"xl"} inline>
-                            Drag images here or click to select files
-                        </Text>
-                        <Text size={"sm"} c={"dimmed"} inline mt={8}>
-                            Attach as many files as you like, each file should not exceed 5mb
-                        </Text>
-                    </div>
-                </Dropzone>
-                <FilePreview filesState={files} setFilesState={fileChangeHandler} />
-                <Button onClick={()=>{createPocketfile(); setFileSubmitted(true)}} mt={"md"}>Submit</Button>
-            </div>
-            {fileSubmitted &&
-                <>
-                    <Divider my={"md"}/>
-                    <Box pos={"relative"}>
-                        <LoadingOverlay visible={imageIsLoading}/> {/*TODO: Figure out how to make this take space when img is off*/}
-                        {imageURL && <img src={imageURL} alt={"Exported Image"} width={500} />}
-                    </Box>
-                    {imageURL && <Button onClick={()=>downloadImage(imageURL)} mt={"md"}>Download</Button>}
-                </>}
+            <AppShell header={{height: 60}}>
+                <AppShellHeader pl={32} pr={32}>
+                    <Group align={"center"} h={"100%"}>
+                        <Title>Pocketverter</Title>
+                    </Group>
+                </AppShellHeader>
+
+                <AppShellMain>
+                    <Container fluid p={0} mb={10}>
+                        <BackgroundImage src={HeroBackground} py={32} px={80}>
+                                <Stack align={"flex-start"} gap={4}>
+                                    <Title c={"white"}>Pocketverter</Title>
+                                    <Text c={"white"}>Convert your printables to a portable format!</Text>
+                                </Stack>
+                        </BackgroundImage>
+                    </Container>
+                    <Group grow align={"flex-start"} gap={32}>
+                        <Stack px={"5%"}>
+                            <div>
+                                <Dropzone onDrop={addFiles} accept={[MIME_TYPES.png,MIME_TYPES.jpeg,MIME_TYPES.pdf]}>
+                                    <div>
+                                        <Text size={"xl"} inline>
+                                            Drag images here or click to select files
+                                        </Text>
+                                        <Text size={"sm"} c={"dimmed"} inline mt={8}>
+                                            Attach as many files as you like, any files beyond 8 pages will be ignored
+                                        </Text>
+                                    </div>
+                                </Dropzone>
+                                <FilePreview filesState={files} setFilesState={fileChangeHandler} />
+                                <Button onClick={()=>{createPocketfile(layoutSubmission); setFileSubmitted(true)}} mt={"md"}>Submit</Button>
+                            </div>
+                            {fileSubmitted &&
+                                <>
+                                    <Divider my={"md"}/>
+                                    <Box pos={"relative"}>
+                                        <LoadingOverlay visible={imageIsLoading}/> {/*TODO: Figure out how to make this take space when img is off*/}
+                                        {imageURL ? <img src={imageURL} alt={"Exported Image"} width={500} /> : <Box c={"gray"} w={500} h={500}></Box>}
+                                    </Box>
+                                    {imageURL && <Button onClick={()=>downloadImage(imageURL)} mt={"md"}>Download</Button>}
+                                </>}
+                        </Stack>
+                        <Stack>
+                            <Select
+                                label={"Layout"}
+                                description={"Select layout of the output file. The preview of the layout will be visible below."}
+                                data={["Pocketmod","Pocketfold"]}
+                                onChange={setLayoutSubmission}
+                                style={{textAlign: "left"}}
+                            />
+                            {layoutSubmission && <Image w={"50%"} src={LayoutPreview(layoutSubmission)} mx={"auto"} />}
+                        </Stack>
+                    </Group>
+                </AppShellMain>
+
+            </AppShell>
         </MantineProvider>
     );
+}
+
+function LayoutPreview(layout: string){
+    if (layout==="Pocketmod")
+        return PocketmodLayout;
+    else if (layout==="Pocketfold")
+        return PocketfoldLayout;
+    else return undefined;
 }
 
 export default App
--- src/ImageConverter.tsx
+++ src/ImageConverter.tsx
@@ -13,11 +13,11 @@ export async function PDFtoIMG(PDFfile: string){
         const page = await pdfDocument.getPage(i + 1);
         const pageViewport = page.getViewport({scale: 4}); //page quality
         const canvas = createCanvas(pageViewport.width, pageViewport.height);
-        console.log(pageViewport.height, pageViewport.height)
+        //console.log(pageViewport.height, pageViewport.height)
         const ctx = canvas.getContext("2d");
         const pageRenderTask = page.render({canvasContext: ctx, viewport: pageViewport});
         await pageRenderTask.promise;
-        console.log(canvas.toDataURL('image/png'));
+        //console.log(canvas.toDataURL('image/png'));
         dataList.push(canvas.toDataURL('image/png'));
     }
     return dataList;
@@ -35,30 +35,74 @@ export async function PDFtoIMG(PDFfile: string){
     return await background.getBuffer("image/png");
 }*/
 
-export async function CanvasLayout(imageList: string[]) {
+type JimpInstance = Awaited<ReturnType<typeof Jimp.read>>; //Jimp bug workaround
+
+interface LayoutPage {
+    page: number,
+    isReversed?: boolean;
+}
+
+interface Layouts {
+    method: string;
+    data: LayoutPage[];
+}
+
+const Layouts: Layouts[] = [
+    { method: "Pocketmod",
+    data: [
+        {page: 2},
+        {page: 3},
+        {page: 4},
+        {page: 5},
+        {page: 1, isReversed: true},
+        {page: 8, isReversed: true},
+        {page: 7, isReversed: true},
+        {page: 6, isReversed: true}
+    ]},
+    { method: "Pocketfold",
+        data: [
+            {page: 7, isReversed: true},
+            {page: 6, isReversed: true},
+            {page: 5, isReversed: true},
+            {page: 4, isReversed: true},
+            {page: 3},
+            {page: 8},
+            {page: 1},
+            {page: 2}
+        ]}
+]
+
+export async function CanvasLayout(imageList: string[], layout: string|null) {
     const background = new Jimp({ width: 3508, height: 2480, color:0xffffffff })
     const partWidth=background.width/4;
     const partHeight=background.height/2;
-    for (let i=0;i<8;i++){
-        if (imageList[i] == undefined) continue;
+    const sortedLayout=[];
+    const pageData=Layouts.find((value: Layouts)=>(value.method===layout));
+    if (pageData===undefined) return; //TODO: Replace with proper error handling
+    for (let i=0;i<pageData.data.length;i++){
         const imgFetch = await fetch(imageList[i]);
         const imgBuffer = await imgFetch.arrayBuffer();
-        const img = await Jimp.read(imgBuffer);
-        let x, y;
-        img.resize({ w: partWidth, h: partHeight });
-        if (i>0 && i<5) {
-            y=0;
-            x=partWidth*(i-1);
-        }
-        else {
+        const img: JimpInstance = await Jimp.read(imgBuffer);
+        const page = pageData.data.find((value: LayoutPage)=>(value.page===i+1))
+        if (page===undefined) return;
+        if (page.isReversed)
             img.rotate(180);
-            y=partHeight;
-            if (i==0)
-                x=0;
-            else x=partWidth*(8-i); //5=3, 6=2, 7=1
-        }
+
+        sortedLayout[pageData.data.indexOf(page)] = img;
+    }
+    for (let i=0;i<sortedLayout.length;i++){
+        if (sortedLayout[i]===undefined) continue;
+        const img: JimpInstance = sortedLayout[i];
+        const [x, y] = RowLayout(partWidth, partHeight, 4, i)
+        img.resize({ w: partWidth, h: partHeight });
         background.composite(img, x, y); //TODO: add x and y testing if they're not undefined
     }
     background.rotate(90);
     return background.getBuffer("image/png");
+}
+
+function RowLayout(partWidth: number, partHeight: number, rowLength: number, i: number){
+    const x=partWidth*(i%rowLength);
+    const y=partHeight*Math.floor(i/rowLength);
+    return [x, y];
 }
